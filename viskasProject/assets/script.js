a456710adaabda1ac4b94afa5695c88ffcad7e5a
TolikTelia <Tol2890@ukr.net>
2020-05-19 19:47:24 +0300

    project viskas 2

--- viskasProject/assets/script.js
+++ viskasProject/assets/script.js
@@ -1,57 +1,59 @@
-window.onload = () =>{
+window.onload = () => {
 
-	function $(className){
-		return document.querySelector(className);
-	}
+    function $(className) {
+        return document.querySelector(className);
+    }
 
 
 ///////slider///////
-let sliderContainer = document.querySelector("#slider ul");
-const numberOfSlides = 6;
-let changeTime = 2000;
-let slideNow = 1;
+    let sliderContainer = document.querySelector("#slider ul");
+    const numberOfSlides = 6;
+    let changeTime = 2000;
+    let slideNow = 1;
 
-function addSlide(max){
-	for (let i = 1; i <= max; i++) {
-	let li = document.createElement("li");
-	let radio = document.createElement("input");
-	radio.type = "radio";
-	radio.name="slide";
-	radio.className = "forSliding";
-	radio.oninput = () =>{
-		slideNow = i;
-	}
-	let label = document.createElement("label");
-	let slideImage = document.createElement("img");
-	
-	let sameImage = `slider${i}`;
-	radio.id = label.htmlFor = slideImage.alt = sameImage;
-	slideImage.src = `../assets/images/${sameImage}.jpg`;
+    function addSlide(max) {
+        for (let i = 1; i <= max; i++) {
+            let li = document.createElement("li");
+            let radio = document.createElement("input");
+            radio.type = "radio";
+            radio.name = "slide";
+            radio.className = "forSliding";
+            radio.oninput = () => {
+                slideNow = i;
+            }
+            let label = document.createElement("label");
+            let slideImage = document.createElement("img");
 
-	sliderContainer.appendChild(li);
+            let sameImage = `slider${i}`;
+            radio.id = label.htmlFor = slideImage.alt = sameImage;
+            slideImage.src = `../assets/images/${sameImage}.jpg`;
 
-	li.append(radio,label,slideImage);
-	}
-}
+            sliderContainer.appendChild(li);
 
-function sliding(max){
-	let slideButtons = document.querySelectorAll(".forSliding");
-	slideButtons[0].checked = true;
-	// console.log(slideButtons);
-	
-	let interval = setInterval(() => {
-		if(slideNow >= max){slideNow = 0} 
-		slideButtons[slideNow].checked = true;
-		slideNow++;
-	}, 3000 + changeTime);
-}
+            li.append(radio, label, slideImage);
+        }
+    }
 
-function slider(max){
-	addSlide(max);
-	sliding(max);
-}
+    function sliding(max) {
+        let slideButtons = document.querySelectorAll(".forSliding");
+        slideButtons[0].checked = true;
+        // console.log(slideButtons);
 
-slider(numberOfSlides);
+        let interval = setInterval(() => {
+            if (slideNow >= max) {
+                slideNow = 0
+            }
+            slideButtons[slideNow].checked = true;
+            slideNow++;
+        }, 3000 + changeTime);
+    }
+
+    function slider(max) {
+        addSlide(max);
+        sliding(max);
+    }
+
+    slider(numberOfSlides);
 ///////slider///////
 
 //////easter-egg////
--- viskasProject/assets/script2.js
+++ viskasProject/assets/script2.js
@@ -1,47 +1,49 @@
+function $(className) {
+    return document.querySelector(className);
+}
 
-	function $(className){
-		return document.querySelector(className);
-	}
 const dude = $('.easter_egg')
 let clicks = 0
 window.clicks = clicks
 const span = $('.easter_egg span')
 console.log(dude)
 dude.onclick = () => {
-	if (clicks < 100) {
-			clicks += 1
-	}
-	span.innerText = `${clicks}/100`
-		if (clicks === 100) {
-												var audio = new Audio('./assets/clicking2.mp3');
-					audio.play();
-		const timeout = setInterval(() => {
-			if (clicks > 0) {
-				apocalipsis()
-				clicks -=1
-				span.innerText = `${clicks}/100`
-			} else {
-				clearTimeout(timeout)
-			}
+    if (clicks < 100) {
+        clicks += 1
+    }
+    span.innerText = `${clicks}/100`
+    if (clicks === 100) {
+        var audio = new Audio('./assets/clicking2.mp3');
+        audio.play();
+        setTimeout(() => {
+            clicks = 130
+            const timeout = setInterval(() => {
+                if (clicks > 0) {
+                    apocalipsis()
+                    clicks -= 1
+                    span.innerText = `${clicks}/100`
+                } else {
+                    clearTimeout(timeout)
+                }
+            }, 50)
+        }, 100)
 
-		}, 50)
-
-	}
+    }
 
 }
 
 const apocalipsis = () => {
-	console.log(clicks)
-	const dudick = document.createElement('img')
-	dudick.src = './assets/images/knuckles2.png'
-	dudick.style.width = 100 + 'px'
-	document.body.appendChild(dudick)
-	dudick.style.position = 'fixed'
-	dudick.style.top = Math.round(Math.random() * 20) + '%'
-		dudick.style.left = Math.round(Math.random() * 98) + '%'
-		dudick.className = 'dudick'
-	
-		setTimeout(() => {
-			document.body.removeChild(dudick)
-		}, 1000)
+    console.log(clicks)
+    const dudick = document.createElement('img')
+    dudick.src = './assets/images/knuckles2.png'
+    dudick.style.width = 100 + 'px'
+    document.body.appendChild(dudick)
+    dudick.style.position = 'fixed'
+    dudick.style.top = Math.round(Math.random() * 20) + '%'
+    dudick.style.left = Math.round(Math.random() * 98) + '%'
+    dudick.className = 'dudick'
+
+    setTimeout(() => {
+        document.body.removeChild(dudick)
+    }, 1000)
 }
